0ca1623b6820cfec4d9735b6192b2f078ab6555d
suresh24krishnan <suresh24.krishnan@gmail.com>
2025-05-12 17:23:15 +0000

    Learning about escape types

---
+++ Escape.java
@@ -0,0 +1,9 @@
+public class Escape {
+    public static void main(String[] args) {
+        String greeting = "Super Mario World aka \"Mario 4\"";
+       
+
+        System.out.println(greeting);
+     
+    }
+}
